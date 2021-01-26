2926499316799da75df5339c5a8329a49def4f61
spoopysnoopy <76850698+spoopysnoopy@users.noreply.github.com>
2021-01-26 10:19:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace New_VSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Average Calculator"; 
+            Console.ForegroundColor = ConsoleColor.Red; 
+            Console.BackgroundColor = ConsoleColor.Black; 
+          
+          
+           double num01;
+           double num02; 
+           double numb03; 
+
+
+           Console.Write("Input a number: ");
+
+           num01 = Convert.ToDouble( Console.ReadLine()  );
+
+           Console.Write("Input a second number:");
+
+           num02 = Convert.ToDouble( Console.ReadLine() );
+
+           Console.Write("Insert a third number: ");
+
+           numb03 = Convert.ToDouble( Console.ReadLine( ));
+
+            double result = (num01 + num02 + numb03) / 3; 
+            Console.WriteLine("The result is " + result );
+
+            Console.ReadKey(); 
+        }
+    }
+}
